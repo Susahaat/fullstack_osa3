8f4afe581140b1b108d13749c9559e68ce93d6dc
Susanna Haataja <susanna.haataja@hotmail.com>
2022-09-08 11:59:10 +0300

    Käsitellään tietoja tietokannan kautta

--- backend/index.js
+++ backend/index.js
@@ -20,80 +20,65 @@ app.use(
 const cors = require("cors");
 app.use(cors());
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 let date = new Date();
 
 app.get("/info", (req, res) => {
-  res.send(
-    "<p>Phonebook has info for " + persons.length + " people.</p>" + date
-  );
+  Person.find({}).then((result) => {
+    res.send(
+      "<p>Phonebook has info for " + result.length + " people.</p>" + date
+    );
+  });
 });
 
 app.get("/api/persons/", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((result) => {
+    res.json(result);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  const id = req.params.id;
+  Person.find({}).then((result) => {
+    const person = result.find((person) => person._id.toJSON() == id);
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+  const id = req.params.id;
+  Person.deleteOne({ _id: id }).then((result) => {
+    res.status(204).end();
+  });
 });
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  const names = persons.map((person) => person.name);
-  const numbers = persons.map((person) => person.number);
+  // const names = persons.map((person) => person.name);
+  // const numbers = persons.map((person) => person.number);
 
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "content missing",
     });
-  } else if (names.includes(body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
   }
+  // } else if (names.includes(body.name)) {
+  //   return res.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 100),
-  };
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT;
